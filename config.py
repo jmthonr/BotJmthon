6ac07a6da9e2b217c413f5cdfc473d948f433e48
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-24 14:03:32 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+TOKEN = os.environ.get("TOKEN", None)
+ID_OWNER = os.environ.get("ID_OWNER", None)
+DB_URI = os.environ.get("DATABASE_URL")
+APP_ID = os.environ.get("APP_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
