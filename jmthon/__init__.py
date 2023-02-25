d354be107fd097f75e92d55b6e5753bad4d097ac
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-26 00:56:16 +0300

    Update __init__.py

--- jmthon/__init__.py
+++ jmthon/__init__.py
@@ -1,18 +1,1 @@
-import glob
-import logging
-from . import plugin_name
-
-from pathlib import Path
-from importlib.util import spec_from_file_location, module_from_spec
-
-def load_plugins(plugin_name):
-    path = Path(f"jmthon/plugins/{plugin_name}.py")
-    name = "jmthon.plugins.{}".format(plugin_name)
-    spec = spec_from_file_location(name, path)
-    load = module_from_spec(spec)
-    spec.loader.exec_module(load)
-    modules["jmthon.plugins." + plugin_name] = load
-    logging.info("🔷 Successfully Imported " + plugin_name)
-    return
-
 
