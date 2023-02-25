34be74c2af524254f5f25ebd9c2d5af1a0c81c47
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-26 02:48:23 +0300

    Update start.py

--- jmthon/plugins/start.py
+++ jmthon/plugins/start.py
@@ -3,9 +3,11 @@ from .. import jmthon
 from . import get_mention
 
 @jmthon.on(events.NewMessage(incoming=True, pattern='/start'))
-async def _startmsg(event):
-    text = f"Hi {get_mention(event)}, I Am Alive."
-    await event.reply(text, buttons=[
+async def _startmsg(e):
+    if not e.is_private:
+        return
+    text = f"Hi {get_mention(e)}, I Am Alive."
+    await e.reply(text, buttons=[
     [Button.url('⭐ Dev ⭐', 'https://t.me/jmthon')],
     [Button.url('❤ Join Channel ❤', 'https://t.me/R0R77')]
 ])
