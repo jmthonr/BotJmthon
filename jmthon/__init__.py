8d2faee167768503e46a9e14dc374b4cc5ed2a03
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-26 02:42:38 +0300

    Update __init__.py

--- jmthon/__init__.py
+++ jmthon/__init__.py
@@ -1,1 +1,17 @@
+from telethon import TelegramClient
+import logging
+from config import APP_ID, API_HASH, TOKEN
 
+logging.basicConfig(
+    format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+    level=logging.INFO
+)
+
+
+jmthon = TelegramClient("jmthon-bot", APP_ID, API_HASH)
+
+try:
+    jmthon.start(bot_token=TOKEN)
+except Exception:
+    print("Bot Token Invalid - التوكن غير صحيح")
+    sys.exit(1)
