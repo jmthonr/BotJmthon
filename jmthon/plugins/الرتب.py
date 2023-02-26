866e5c96243f432b08eaef0148fd78e088142224
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-26 16:17:24 +0300

    Create الرتب.py

---
+++ jmthon/plugins/الرتب.py
@@ -0,0 +1,15 @@
+from telethon import events
+
+from ..database.momez import add_momez 
+from jmthon import jmthon
+
+@client.on(events.NewMessage(pattern='^رفع مميز$'))
+async def add_momez_handler(event):
+    sender_id = event.sender_id
+    chat_id = event.chat_id
+    if event.is_reply:
+        reply_msg = await event.get_reply_message()
+        if reply_msg.sender_id:
+            sender_id = reply_msg.sender_id
+    add_momez(sender_id, chat_id)
+    await event.respond(f"المستخدم تم رفعه مميز بنجاح {sender_id}")
