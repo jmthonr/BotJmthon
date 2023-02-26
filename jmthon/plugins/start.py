b68360f795bd230eb00bbf4fe74054fa3607e93c
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-26 03:02:45 +0300

    Update start.py

--- jmthon/plugins/start.py
+++ jmthon/plugins/start.py
@@ -1,4 +1,5 @@
 from telethon import events, Button
+from ..database import add, get
 from .. import jmthon
 from . import get_mention
 
@@ -12,3 +13,14 @@ async def _startmsg(e):
     [Button.url('❤ Join Channel ❤', 'https://t.me/R0R77')]
 ])
     return
+
+
+
+@jmthon.on(events.NewMessage(incoming=True, pattern='^تفعيل'))
+async def enable_group(event):
+    chat_id = event.chat_id
+    if get("TF3EL"):
+        await event.reply("ألمجموعة مفعلة بنجاح")
+    else:
+        add("TF3EL", "Done")
+        await event.reply("تم بنجاح تفعيل المجموعة بنجاح")
