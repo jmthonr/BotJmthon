37446a85c058f414abb0650b01444e971a658c1c
jmthonr <103285401+jmthonr@users.noreply.github.com>
2023-02-28 12:49:49 +0300

    Update momez.py

--- jmthon/database/momez.py
+++ jmthon/database/momez.py
@@ -1,32 +1,59 @@
-try:
-    from . import BASE, SESSION
-except ImportError as e:
-    raise Exception("Hello!") from e
+import threading
+from . import BASE, SESSION
+
 from sqlalchemy import Column, String
+from sqlalchemy.sql.sqltypes import BigInteger
+
 
 
 class Momez(BASE):
     __tablename__ = "momez"
-    sender_id = Column(String(14), primary_key=True)
     chat_id = Column(String(14), primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
 
-    def __init__(self, sender_id, chat_id):
-        self.sender_id = str(sender_id)
+    def __init__(self, chat_id, user_id):
         self.chat_id = str(chat_id)
+        self.user_id = user_id
 
-def add_momez(sender_id, chat_id):
-    momez = Momez(str(sender_id), str(chat_id))
-    SESSION.add(momez)
-    SESSION.commit()
+    def __repr__(self):
+        return "<Setmomez %s>" % self.user_id
 
-    
-def del_momez(sender_id, chat_id):
-    if momez := SESSION.query(Momez).get((str(sender_id), str(chat_id))):
-        SESSION.delete(momez)
+
+Momez.__table__.create(checkfirst=True)
+
+MOMEZ_INSERTION_LOCK = threading.RLock()
+
+
+def setmomez(chat_id, user_id):
+    with MOMEZ_INSERTION_LOCK:
+        momez_user = Momez(str(chat_id), user_id)
+        SESSION.add(momez_user)
         SESSION.commit()
 
 
-def get_momez_list():
-    momezes = SESSION.query(Momez).all()
-    SESSION.close()
-    return [(momez.sender_id, momez.chat_id) for momez in momezes]
+def is_momez(chat_id, user_id):
+    try:
+        return SESSION.query(Momez).get((str(chat_id), user_id))
+    finally:
+        SESSION.close()
+
+
+def delmomez(chat_id, user_id):
+    with MOMEZ_INSERTION_LOCK:
+        delmomez_user = SESSION.query(Momez).get((str(chat_id), user_id))
+        if delmomez_user:
+            SESSION.delete(delmomez_user)
+            SESSION.commit()
+            return True
+        else:
+            SESSION.close()
+            return False
+
+
+def list_momez(chat_id):
+    try:
+        return (SESSION.query(Momez).filter(
+            Momez.chat_id == str(chat_id)).order_by(
+                Momez.user_id.asc()).all())
+    finally:
+        SESSION.close()
